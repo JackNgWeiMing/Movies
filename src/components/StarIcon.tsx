d3cf153b78e2b172ce07ca36424ab6dc885e8ee2
jackng <jackng9995@gmail.com>
2022-03-25 17:42:57 +0800

    just formating

--- src/components/StarIcon.tsx
+++ src/components/StarIcon.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import {Image} from 'react-native';
-import Svg, {G, Path, Defs, ClipPath, SvgProps} from 'react-native-svg';
+import {Image, StyleSheet} from 'react-native';
 /* SVGR has dropped some elements not supported by react-native-svg: filter */
 
 export function StarIcon() {
   return (
     <Image
       source={require('../images/star.png')}
-      style={{width: 15, height: 15}}
+      style={styles.root}
       resizeMode="contain"
     />
   );
 }
+
+const styles = StyleSheet.create({
+  root: {width: 15, height: 15},
+});
 
 export default StarIcon;
